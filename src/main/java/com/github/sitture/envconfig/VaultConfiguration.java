641f91a9e7c6aa073514f0874771f751dcbdaca4
Richard Heal <richard.heal@sky.uk>
2024-02-28 14:48:26 +0000

    Reduce validateToken complexity

--- src/main/java/com/github/sitture/envconfig/VaultConfiguration.java
+++ src/main/java/com/github/sitture/envconfig/VaultConfiguration.java
@@ -39,20 +39,24 @@ class VaultConfiguration {
                 this.vault.auth().lookupSelf();
                 break;
             } catch (VaultException vaultException) {
-                final long retryInterval = i * 2L;
-                logError(String.format("An exception occurred validating the vault token, will retry in %s seconds", retryInterval), vaultException);
-                try {
-                    TimeUnit.SECONDS.sleep(retryInterval);
-                } catch (InterruptedException ex) {
-                    logError("InterruptedException thrown whilst waiting to retry validating the vault token", ex);
-                }
-                if (i == validateTokenMaxRetries - 1) {
-                    final String message = String.format("Reached CONFIG_VAULT_VALIDATE_MAX_RETRIES limit (%s) attempting to validate token", validateTokenMaxRetries);
-                    logError(message, vaultException);
-
-                    throw new EnvConfigException(message, vaultException);
-                }
+                retryUntilMaxMaxRetries(vaultException, i, validateTokenMaxRetries);
             }
+        }
+    }
+
+    private static void retryUntilMaxMaxRetries(VaultException vaultException, int i, int validateTokenMaxRetries) {
+        final long retryInterval = i * 2L;
+        logError(String.format("An exception occurred validating the vault token, will retry in %s seconds", retryInterval), vaultException);
+        try {
+            TimeUnit.SECONDS.sleep(retryInterval);
+        } catch (InterruptedException ex) {
+            logError("InterruptedException thrown whilst waiting to retry validating the vault token", ex);
+        }
+        if (i == validateTokenMaxRetries - 1) {
+            final String message = String.format("Reached CONFIG_VAULT_VALIDATE_MAX_RETRIES limit (%s) attempting to validate token", validateTokenMaxRetries);
+            logError(message, vaultException);
+
+            throw new EnvConfigException(message, vaultException);
         }
     }
 
